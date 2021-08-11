b50b45ae71bfea0cbe6c4e6f931fe399f3fd257c
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2021-08-11 01:28:44 -0700

    Time: 36 ms (97.21%), Space: 13.6 MB (82.73%) - LeetHub

--- zigzag-conversion/zigzag-conversion.py
+++ zigzag-conversion/zigzag-conversion.py
@@ -8,14 +8,14 @@ class Solution(object):
         if numRows == 1:
             return s
         
-        line = 0
+        row = 0
         outputStr = [""] * numRows
         direction = 1
 
         for i in range(len(s)):
-            outputStr[line] += s[i]
-            line += direction
-            if line == numRows-1 or line == 0:
+            outputStr[row] += s[i]
+            row += direction
+            if row == numRows-1 or row == 0:
                 direction *= -1
         
         return ''.join(outputStr)
