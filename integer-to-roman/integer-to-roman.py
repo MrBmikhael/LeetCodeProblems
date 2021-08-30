0f4b0a255580036e3c11d592c15b5e0f82a423cd
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2021-08-30 10:10:50 -0700

    Time: 54 ms (20.76%), Space: 13.5 MB (14.38%) - LeetHub

--- integer-to-roman/integer-to-roman.py
+++ integer-to-roman/integer-to-roman.py
@@ -4,58 +4,13 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        
+        roman = [{'M':1000}, {'CM':900}, {'D':500}, {'CD':400}, {'C':100}, {'XC':90}, {'L': 50}, {'XL':40}, {'X': 10}, {'IX': 9}, {'V':5}, {'IV': 4}, {'I':1}]
         output = []
         
-        while num >= 1000:
-            output.append('M')
-            num -= 1000
-            
-        if num >= 900:
-            output.append('CM')
-            num -= 900
-            
-        if num >= 500:
-            output.append('D')
-            num -= 500
-            
-        if num >= 400:
-            output.append('CD')
-            num -= 400
-            
-        while num >= 100:
-            output.append('C')
-            num -= 100
-            
-        if num >= 90:
-            output.append('XC')
-            num -= 90
-        
-        if num >= 50:
-            output.append('L')
-            num -= 50
-        
-        if num >= 40:
-            output.append('XL')
-            num -= 40
-            
-        while num >= 10:
-            output.append('X')
-            num -= 10
-            
-        if num >= 9:
-            output.append('IX')
-            num -= 9
-            
-        if num >= 5:
-            output.append('V')
-            num -= 5
-            
-        if num >= 4:
-            output.append('IV')
-            num -= 4
-            
-        while num > 0:
-            output.append('I')
-            num -= 1
-            
+        for i in roman:
+            k, v = i.items()[0]
+            while num >= v:
+                output.append(k)
+                num -= v
         return ''.join(output)
