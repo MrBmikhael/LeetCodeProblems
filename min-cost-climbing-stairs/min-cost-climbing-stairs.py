e41a66498b17f91a698fe079e6359395bfb36f40
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2021-09-10 14:58:54 -0700

    Time: 84 ms (5.85%), Space: 13.6 MB (30.32%) - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,21 +1,15 @@
 class Solution(object):
-    def minCost(self, cost, i, dp):
-        if i == 0 or i == 1:
-            return cost[i]
-        elif i < 0:
-            return 0
-        if dp[i] != None:
-            return dp[i]
-        dp[i] = cost[i] + min(self.minCost(cost, i-1, dp), self.minCost(cost, i-2, dp))
-        return dp[i]
-    
     def minCostClimbingStairs(self, cost):
         """
         :type cost: List[int]
         :rtype: int
         """
+        dp = [cost[0], cost[1]]
         n = len(cost)
-        dp = [None] * n
-        return min(self.minCost(cost, n-1, dp), self.minCost(cost, n-2, dp))
+
+        for i in range(2,n):
+            dp[i%2] = cost[i] + min(dp[(i%2)-1], dp[(i%2)-2])
+        
+        return min(dp)
         
         
