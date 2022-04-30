d18537097ae1f568c8c87f21133784cfc037fa2b
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2022-04-30 10:54:47 -0700

    Time: 50 ms (82.38%), Space: 14.5 MB (24.26%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        compDict = {}
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if nums[i] in compDict:
+                return [compDict[nums[i]], i]
+            compDict[comp] = i
