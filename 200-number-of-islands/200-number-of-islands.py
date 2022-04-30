6d04dd68f0557a7ed10ddec6d10bb034868c5f52
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2022-04-30 11:14:33 -0700

    Time: 443 ms (30.27%), Space: 28.6 MB (92.68%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        queue = list()
+        islands = 0
+        directionsArr = [[-1,0],[0,1],[0,-1],[1,0]]
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                    if grid[i][j] == "1":
+                        islands += 1
+                        queue.append([i,j])
+                        while queue:
+                            location = queue.pop(0)
+                            for d in directionsArr:
+                                newLoc = [d[0]+location[0], d[1]+location[1]]
+                                if newLoc[0] >= 0 and newLoc[0] < len(grid) and newLoc[1] >= 0 and newLoc[1] < len(grid[0]):
+                                    if grid[newLoc[0]][newLoc[1]] == "1":
+                                        queue.append(newLoc)
+                                        grid[newLoc[0]][newLoc[1]] = "0"
+        return islands
